48e67198d0ec55a187d141ee9018d8d20f00ff7b
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-17 10:01:49 -0300

    webapi:refactor: cmd to create the database connection and provide the monitoring into the container

--- webapi/cmd/webapi_cmd.go
+++ webapi/cmd/webapi_cmd.go
@@ -1,11 +1,9 @@
 package cmd
 
 import (
-	"os"
 	"webapi/pkg/infra/environments"
 
 	"github.com/newrelic/go-agent/v3/integrations/nrgin"
-	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 func WebApi() error {
@@ -13,25 +11,13 @@ func WebApi() error {
 		return err
 	}
 
-	app, err := newrelic.NewApplication(
-		newrelic.ConfigAppName(os.Getenv("APP_NAME")),
-		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
-		// newrelic.ConfigDebugLogger(os.Stdout),
-		newrelic.ConfigDistributedTracerEnabled(true),
-		func(cfg *newrelic.Config) {
-			cfg.CustomInsightsEvents.Enabled = true
-		},
-	)
-	if err != nil {
-		return err
-	}
 	container := NewContainer()
 
 	// Server setup
 	container.httpServer.Setup()
 
 	//
-	container.httpServer.Use(nrgin.Middleware(app))
+	container.httpServer.Use(nrgin.Middleware(container.monitoring))
 
 	// Router register
 	container.usersRoutes.Register(container.httpServer)
--- webapi/cmd/webapi_ioc.go
+++ webapi/cmd/webapi_ioc.go
@@ -1,13 +1,18 @@
 package cmd
 
 import (
+	"os"
 	"webapi/pkg/app/interfaces"
 	appUseCases "webapi/pkg/app/usecases"
 	domainUseCases "webapi/pkg/domain/usecases"
+	"webapi/pkg/infra/database"
 	httpServer "webapi/pkg/infra/http_server"
 	"webapi/pkg/infra/logger"
+	"webapi/pkg/infra/repositories"
 	"webapi/pkg/interfaces/http/handlers"
 	"webapi/pkg/interfaces/http/presenters"
+
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 type webApiContainer struct {
@@ -17,13 +22,40 @@ type webApiContainer struct {
 	createUserUseCase domainUseCases.ICreateUserUseCase
 	usersHandler      handlers.IUsersHandler
 	usersRoutes       presenters.IUsersRoutes
+
+	monitoring *newrelic.Application
 }
 
 func NewContainer() webApiContainer {
+	monitoring, err := newrelic.NewApplication(
+		newrelic.ConfigAppName(os.Getenv("APP_NAME")),
+		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
+		// newrelic.ConfigDebugLogger(os.Stdout),
+		newrelic.ConfigDistributedTracerEnabled(true),
+		func(cfg *newrelic.Config) {
+			cfg.CustomInsightsEvents.Enabled = true
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	dbConnection, err := database.GetConnection(
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
 	logger := logger.NewLogger()
 	httpServer := httpServer.NewHttpServer(logger)
 
-	createUserUseCase := appUseCases.NewCreateUserUseCase()
+	userRepository := repositories.NewUserRepository(dbConnection, monitoring)
+	createUserUseCase := appUseCases.NewCreateUserUseCase(userRepository)
 	usersHandler := handlers.NewUsersHandler(logger, createUserUseCase)
 	usersRoutes := presenters.NewUsersRoutes(logger, usersHandler)
 
@@ -34,5 +66,7 @@ func NewContainer() webApiContainer {
 		createUserUseCase: createUserUseCase,
 		usersHandler:      usersHandler,
 		usersRoutes:       usersRoutes,
+
+		monitoring: monitoring,
 	}
 }
