6a35b7949f054566425a10b5d81681b7ce2aa922
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-18 05:58:08 -0300

    webapi:refactor: user repository

--- webapi/pkg/app/interfaces/i_user_repository.go
+++ webapi/pkg/app/interfaces/i_user_repository.go
@@ -2,10 +2,11 @@ package interfaces
 
 import (
 	"context"
+	"webapi/pkg/domain/dtos"
 	"webapi/pkg/domain/entities"
 )
 
 type IUserRepository interface {
 	FindByEmail(ctx context.Context, email string) (*entities.User, error)
-	Create(ctx context.Context) (*entities.User, error)
+	Create(ctx context.Context, dto dtos.CreateUserDto) (*entities.User, error)
 }
