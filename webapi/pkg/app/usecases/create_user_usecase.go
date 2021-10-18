49eca73e22e003bffe8a48d49960862979774ac7
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-18 05:59:06 -0300

    webapi:refactor: create user usecase

--- webapi/pkg/app/usecases/create_user_usecase.go
+++ webapi/pkg/app/usecases/create_user_usecase.go
@@ -2,6 +2,7 @@ package usecases
 
 import (
 	"context"
+	"webapi/pkg/app/errors"
 	"webapi/pkg/app/interfaces"
 	"webapi/pkg/domain/dtos"
 	"webapi/pkg/domain/entities"
@@ -13,8 +14,21 @@ type createUserUseCase struct {
 }
 
 func (pst createUserUseCase) CreateUser(ctx context.Context, dto dtos.CreateUserDto) (entities.User, error) {
-	pst.repository.FindByEmail(ctx, dto.Email)
-	return entities.User{}, nil
+	user, err := pst.repository.FindByEmail(ctx, dto.Email)
+	if err != nil {
+		return entities.User{}, errors.NewInternalError(err.Error())
+	}
+
+	if user != nil {
+		return entities.User{}, errors.NewConflictError("Email already registered")
+	}
+
+	user, err = pst.repository.Create(ctx, dto)
+	if err != nil {
+		return entities.User{}, errors.NewInternalError(err.Error())
+	}
+
+	return *user, nil
 }
 
 func NewCreateUserUseCase(repository interfaces.IUserRepository) usecases.ICreateUserUseCase {
