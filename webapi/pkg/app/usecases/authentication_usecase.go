44d27bad2c7a7b74c68c7ec9e8e5f8edae0a1035
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-19 07:54:46 -0300

    webapi:feat: authentication usecase

---
+++ webapi/pkg/app/usecases/authentication_usecase.go
@@ -0,0 +1,53 @@
+package usecases
+
+import (
+	"context"
+	"os"
+	"time"
+	"webapi/pkg/app/errors"
+	"webapi/pkg/app/interfaces"
+	"webapi/pkg/domain/dtos"
+	"webapi/pkg/domain/usecases"
+)
+
+type authenticationUsecase struct {
+	repository   interfaces.IUserRepository
+	hasher       interfaces.IHasher
+	tokenManager interfaces.ITokenManager
+}
+
+func (pst authenticationUsecase) Perform(ctx context.Context, dto dtos.AuthenticationDto) (dtos.AuthenticatedUserDto, error) {
+	user, err := pst.repository.FindByEmail(ctx, dto.Email)
+	if err != nil {
+		return dtos.AuthenticatedUserDto{}, err
+	}
+
+	if !pst.hasher.Verify(dto.Password, user.Password) {
+		return dtos.AuthenticatedUserDto{}, errors.NewBadRequestError("Wrong password")
+	}
+
+	expireIn := time.Now().Add(time.Hour)
+	accessToken, err := pst.tokenManager.GenerateToken(dtos.TokenDataDto{
+		Id:       user.Id,
+		Audience: os.Getenv("WebApi"),
+		ExpireIn: expireIn,
+	})
+	if err != nil {
+		return dtos.AuthenticatedUserDto{}, err
+	}
+
+	return dtos.AuthenticatedUserDto{
+		AccessToken: accessToken,
+		Kind:        os.Getenv("TOKEN_KIND"),
+		ExpireIn:    expireIn,
+	}, nil
+
+}
+
+func NewAuthenticationUseCase(repository interfaces.IUserRepository, hasher interfaces.IHasher, tokenManager interfaces.ITokenManager) usecases.IAuthenticationUseCase {
+	return authenticationUsecase{
+		repository:   repository,
+		hasher:       hasher,
+		tokenManager: tokenManager,
+	}
+}
--- webapi/pkg/domain/usecases/i_authentication_usecase.go
+++ webapi/pkg/domain/usecases/i_authentication_usecase.go
@@ -1,1 +1,10 @@
 package usecases
+
+import (
+	"context"
+	"webapi/pkg/domain/dtos"
+)
+
+type IAuthenticationUseCase interface {
+	Perform(ctx context.Context, dto dtos.AuthenticationDto) (dtos.AuthenticatedUserDto, error)
+}
