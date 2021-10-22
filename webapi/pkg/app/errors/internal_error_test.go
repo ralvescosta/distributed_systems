ae929918798e1d449415e70378519e8ca2ea72f1
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-22 06:25:33 -0300

    webapi:test: internal error err

--- webapi/pkg/app/errors/internal_error_test.go
+++ webapi/pkg/app/errors/internal_error_test.go
@@ -9,5 +9,5 @@ import (
 func Test_Should_Create_InternalError(t *testing.T) {
 	err := NewInternalError("internal error")
 
-	assert.EqualError(t, err, "conflict error", "the error message must be the same message when the error was created")
+	assert.EqualError(t, err, "internal error", "the error message must be the same message when the error was created")
 }
