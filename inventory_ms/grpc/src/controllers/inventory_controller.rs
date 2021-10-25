c863be467d27a6ca4170bf948d3c4bed4c6d23c7
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-25 07:35:54 -0300

    inventory:feat: controller

--- inventory_ms/grpc/src/controllers/inventory_controller.rs
+++ inventory_ms/grpc/src/controllers/inventory_controller.rs
@@ -1,39 +1,66 @@
+use std::sync::Arc;
 use tonic::{Request, Response, Status};
 
 use crate::inventory::{
     inventory_server::Inventory, CreateProductRequest, Empty, GetByIdRequest, ProductResponse,
     ProductsResponse, UpdateProductRequest,
 };
+use domain::usecases::i_get_inventory_by_id::IGetInventoryByIdUseCase;
 
-#[derive(Default)]
-pub struct InventoryController;
+pub struct InventoryController {
+    get_product_by_id_use_case: Arc<dyn IGetInventoryByIdUseCase>,
+}
+
+unsafe impl Sync for InventoryController {}
+unsafe impl Send for InventoryController {}
+
+impl InventoryController {
+    pub fn new(
+        get_product_by_id_use_case: Arc<dyn IGetInventoryByIdUseCase>,
+    ) -> InventoryController {
+        InventoryController {
+            get_product_by_id_use_case,
+        }
+    }
+}
 
 #[tonic::async_trait]
 impl Inventory for InventoryController {
     async fn get_product_by_id(
         &self,
-        request: Request<GetByIdRequest>,
+        _request: Request<GetByIdRequest>,
     ) -> Result<Response<ProductResponse>, Status> {
-        todo!()
+        self.get_product_by_id_use_case.perform();
+
+        Ok(Response::new(ProductResponse {
+            id: 1,
+            tag: String::new(),
+            title: String::new(),
+            subtitle: String::new(),
+            authors: vec![],
+            amount_in_stock: 10,
+            created_at: String::new(),
+            updated_at: String::new(),
+        }))
     }
 
     async fn get_products(
         &self,
-        request: Request<Empty>,
+        _request: Request<Empty>,
     ) -> Result<Response<ProductsResponse>, Status> {
         todo!()
     }
 
     async fn create_product(
         &self,
-        request: Request<CreateProductRequest>,
+        _request: Request<CreateProductRequest>,
     ) -> Result<Response<ProductResponse>, Status> {
         todo!()
     }
 
     async fn update_product(
         &self,
-        request: Request<UpdateProductRequest>,
+        _request: Request<UpdateProductRequest>,
     ) -> Result<Response<ProductResponse>, Status> {
         todo!()
     }
