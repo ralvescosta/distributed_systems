48fd92f7f85e1efbeb8024d1b62d1b7734a222fb
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-25 07:33:50 -0300

    webapi:feat: inventory client

--- webapi/pkg/infra/grpc_clients/inventory_client.go
+++ webapi/pkg/infra/grpc_clients/inventory_client.go
@@ -1,1 +1,34 @@
-package grpc_client
+package clients
+
+import (
+	"context"
+	"fmt"
+	"webapi/pkg/app/interfaces"
+	"webapi/pkg/infra/grpc_clients/proto"
+
+	"google.golang.org/grpc"
+)
+
+type inventoryClient struct {
+	conn   grpc.ClientConn
+	logger interfaces.ILogger
+}
+
+func (pst inventoryClient) GetInventoryById() {
+	client := proto.NewInventoryClient(&pst.conn)
+
+	response, err := client.GetProductById(context.Background(), &proto.GetByIdRequest{
+		Id: 1,
+	})
+
+	pst.logger.Debug("[InventoryClient::GetInventoryById]")
+	pst.logger.Debug(fmt.Sprintf("%v", response))
+	pst.logger.Debug(err.Error())
+}
+
+func NewInventoryClient(conn grpc.ClientConn, logger interfaces.ILogger) interfaces.IIventoryClient {
+	return inventoryClient{
+		conn:   conn,
+		logger: logger,
+	}
+}
