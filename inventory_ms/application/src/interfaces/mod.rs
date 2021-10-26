755318be3192f1d3d5f251ca537edd03fb7820c2
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-26 04:31:27 -0300

    inventory:refactor: interface

---
+++ inventory_ms/application/src/interfaces/mod.rs
@@ -0,0 +1,1 @@
+pub mod i_logger;
--- inventory_ms/domain/src/usecases/i_get_inventory_by_id.rs
+++ inventory_ms/domain/src/usecases/i_get_inventory_by_id.rs
@@ -1,15 +1,3 @@
 pub trait IGetInventoryByIdUseCase {
     fn perform(&self);
 }
-
-impl IGetInventoryByIdUseCase for dyn Send {
-    fn perform(&self) {
-        todo!()
-    }
-}
-
-impl IGetInventoryByIdUseCase for dyn Sync {
-    fn perform(&self) {
-        todo!()
-    }
-}
