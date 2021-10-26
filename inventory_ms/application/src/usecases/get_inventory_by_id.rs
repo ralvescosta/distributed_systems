030ad3b18d263394589ed8c36504a86fc1d8c485
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-26 04:31:40 -0300

    inventory:refactor: usecase

--- inventory_ms/application/src/usecases/get_inventory_by_id.rs
+++ inventory_ms/application/src/usecases/get_inventory_by_id.rs
@@ -1,15 +1,22 @@
+use std::sync::Arc;
+
 use domain::usecases::i_get_inventory_by_id::IGetInventoryByIdUseCase;
 
-pub struct GetInventoryByIdUseCase {}
+use crate::interfaces::i_logger::ILogger;
+
+pub struct GetInventoryByIdUseCase {
+    logger: Arc<dyn ILogger>,
+}
 
 impl GetInventoryByIdUseCase {
-    pub fn new() -> GetInventoryByIdUseCase {
-        GetInventoryByIdUseCase {}
+    pub fn new(logger: Arc<dyn ILogger>) -> GetInventoryByIdUseCase {
+        GetInventoryByIdUseCase { logger }
     }
 }
 
 impl IGetInventoryByIdUseCase for GetInventoryByIdUseCase {
     fn perform(&self) {
-        println!("[GetInventoryByIdUseCase::perform]")
+        self.logger
+            .debug("[GetInventoryByIdUseCase::perform]", "Request");
     }
 }
