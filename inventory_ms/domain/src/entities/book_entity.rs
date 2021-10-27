1a690df448a7ae9d7af1f91811627f42b735dee7
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-27 07:40:16 -0300

    inventory:feat: book entity

---
+++ inventory_ms/domain/src/entities/book_entity.rs
@@ -0,0 +1,11 @@
+pub struct BookEntity {
+    pub tag: String,
+    pub title: String,
+    pub subtitle: String,
+    pub authors: Vec<String>,
+    pub amount_in_stock: i32,
+    pub created_at: String,
+    pub updated_at: String,
+    pub num_pages: i32,
+    pub tags: Vec<String>,
+}
