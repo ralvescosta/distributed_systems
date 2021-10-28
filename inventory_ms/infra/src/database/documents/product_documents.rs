3bd367a8e7ba775f1fa4040ff79094eacaf3944a
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-28 04:43:07 -0300

    inventory:refactor: proto

--- inventory_ms/infra/src/database/documents/product_documents.rs
+++ inventory_ms/infra/src/database/documents/product_documents.rs
@@ -1,9 +1,10 @@
+use domain::entities::product_entity::ProductEntity;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct ProductDocument {
     pub id: String,
-    pub tg: String,
+    pub product_category: String,
     pub tag: String,
     pub title: String,
     pub subtitle: String,
@@ -13,4 +14,22 @@ pub struct ProductDocument {
     pub updated_at: String,
     pub num_pages: i32,
     pub tags: Vec<String>,
+}
+
+impl ProductDocument {
+    pub fn to_entity(self) -> ProductEntity {
+        ProductEntity {
+            id: self.id,
+            product_category: self.product_category,
+            tag: self.tag,
+            title: self.title,
+            subtitle: self.subtitle,
+            authors: self.authors,
+            amount_in_stock: self.amount_in_stock,
+            created_at: self.created_at,
+            updated_at: self.updated_at,
+            num_pages: self.num_pages,
+            tags: self.tags,
+        }
+    }
 }
