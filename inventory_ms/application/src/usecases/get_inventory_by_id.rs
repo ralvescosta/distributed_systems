5cc87fdb0b25da1889f6156c5703775d48f9ddde
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-28 04:46:10 -0300

    inventory:feat: inventory usecase

--- inventory_ms/application/src/usecases/get_inventory_by_id.rs
+++ inventory_ms/application/src/usecases/get_inventory_by_id.rs
@@ -1,21 +1,28 @@
+use async_trait::async_trait;
 use std::sync::Arc;
 
 use domain::usecases::i_get_inventory_by_id::IGetInventoryByIdUseCase;
 
-use crate::interfaces::i_logger::ILogger;
+use crate::interfaces::{i_logger::ILogger, i_product_repository::IProductRepository};
 
 pub struct GetInventoryByIdUseCase {
     logger: Arc<dyn ILogger>,
+    repo: Arc<dyn IProductRepository>,
 }
 
 impl GetInventoryByIdUseCase {
-    pub fn new(logger: Arc<dyn ILogger>) -> GetInventoryByIdUseCase {
-        GetInventoryByIdUseCase { logger }
+    pub fn new(
+        logger: Arc<dyn ILogger>,
+        repo: Arc<dyn IProductRepository>,
+    ) -> GetInventoryByIdUseCase {
+        GetInventoryByIdUseCase { logger, repo }
     }
 }
 
+#[async_trait]
 impl IGetInventoryByIdUseCase for GetInventoryByIdUseCase {
-    fn perform(&self) {
+    async fn perform(&self) {
+        let _product = self.repo.get_product_by_id("1234".to_string()).await;
         self.logger
             .debug("GetInventoryByIdUseCase::perform", "Request");
     }
