2b22a57e20197139977b5230c9cb918d6980055a
ralvescosta <rafael.rac.mg@gmail.com>
2021-10-29 07:11:44 -0300

    inventory:refactor: refactor usecase

--- inventory_ms/domain/src/usecases/i_get_inventory_by_id.rs
+++ inventory_ms/domain/src/usecases/i_get_inventory_by_id.rs
@@ -1,6 +1,9 @@
 use async_trait::async_trait;
+use std::error::Error;
+
+use crate::entities::product_entity::ProductEntity;
 
 #[async_trait]
 pub trait IGetInventoryByIdUseCase: Send + Sync {
-    async fn perform(&self);
+    async fn perform(&self, id: String) -> Result<Option<ProductEntity>, Box<dyn Error>>;
 }
