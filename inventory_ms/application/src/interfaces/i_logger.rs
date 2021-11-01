f6449a722681ccd00cf574f62a2aeae2e83fac3e
ralvescosta <rafael.rac.mg@gmail.com>
2021-11-01 07:15:37 -0300

    inventory:refator: remove personal logger

--- inventory_ms/application/src/interfaces/i_logger.rs
+++
@@ -1,7 +0,0 @@
-pub trait ILogger: Send + Sync {
-    fn trace(&self, target: &str, msg: &str);
-    fn debug(&self, target: &str, msg: &str);
-    fn info(&self, target: &str, msg: &str);
-    fn warn(&self, target: &str, msg: &str);
-    fn error(&self, target: &str, msg: &str);
-}
