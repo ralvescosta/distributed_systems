bc745caee0a45531d2d78ab524d0e23bff03bce2
ralvescosta <rafael.rac.mg@gmail.com>
2021-11-03 07:42:30 -0300

    webapi:feat: adapters helper unit test

--- webapi/pkg/infra/adapters/helper_test.go
+++ webapi/pkg/infra/adapters/helper_test.go
@@ -1,1 +1,25 @@
 package adapters
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"webapi/pkg/interfaces/http"
+)
+
+func Test_Should_Build_Http_Request_Correctly(t *testing.T) {
+	ginCtx := createMockedGinContext(createMockedHttpRequest(false))
+	request, err := GetHttpRequest(ginCtx)
+
+	assert.NoError(t, err)
+	assert.IsType(t, request, http.HttpRequest{})
+}
+
+func Test_Should_Return_Err_If_Some_Error_Occur_In_Body_Reader(t *testing.T) {
+	ginCtx := createMockedGinContext(createMockedHttpRequest(true))
+	request, err := GetHttpRequest(ginCtx)
+
+	assert.Error(t, err)
+	assert.Nil(t, request.Body)
+}
