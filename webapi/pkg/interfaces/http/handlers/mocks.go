51780441733daf012257d7caeb9b9f7c48c51a92
ralvescosta <rafael.rac.mg@gmail.com>
2021-11-03 08:04:07 -0300

    webapi:feat: user handler test

--- webapi/pkg/interfaces/http/handlers/mocks.go
+++ webapi/pkg/interfaces/http/handlers/mocks.go
@@ -6,31 +6,50 @@ import (
 	"webapi/pkg/domain/dtos"
 	"webapi/pkg/domain/usecases"
 	"webapi/pkg/infra/logger"
+	"webapi/pkg/interfaces/http/models"
 )
 
 type userHandlerToTest struct {
-	handler   IUsersHandler
-	loggerSpy interfaces.ILogger
-	useCase   usecases.ICreateUserUseCase
+	handler    IUsersHandler
+	loggerSpy  interfaces.ILogger
+	useCase    usecases.ICreateUserUseCase
+	mockedUser models.CreateUserRequest
 }
 
-func newUserHandlerToTest() userHandlerToTest {
+func newUserHandlerToTest(validationFailure bool, useCaseError error) userHandlerToTest {
 	loggerSpy := logger.NewLoggerSpy()
-	useCase := createUserUseCaseSpy{}
-	validatorSpy := _validatorSpy{}
+	useCase := createUserUseCaseSpy{useCaseError}
+	validatorSpy := _validatorSpy{validationFailure}
 	handler := NewUsersHandler(loggerSpy, useCase, validatorSpy)
 
-	return userHandlerToTest{handler, loggerSpy, useCase}
+	mockedUser := models.CreateUserRequest{
+		Name:     "Some Name",
+		Email:    "some@email.com",
+		Password: "1234567",
+	}
+
+	return userHandlerToTest{handler, loggerSpy, useCase, mockedUser}
 }
 
-type _validatorSpy struct{}
+type _validatorSpy struct {
+	validationFailure bool
+}
 
-func (_validatorSpy) ValidateStruct(m interface{}) []dtos.ValidatedDto {
+func (pst _validatorSpy) ValidateStruct(m interface{}) []dtos.ValidatedDto {
+	if pst.validationFailure {
+		return []dtos.ValidatedDto{
+			{
+				IsValid: false,
+			},
+		}
+	}
 	return nil
 }
 
-type createUserUseCaseSpy struct{}
+type createUserUseCaseSpy struct {
+	useCaseError error
+}
 
-func (createUserUseCaseSpy) Perform(ctx context.Context, txn interface{}, dto dtos.CreateUserDto) (dtos.CreatedUserDto, error) {
-	return dtos.CreatedUserDto{}, nil
+func (pst createUserUseCaseSpy) Perform(ctx context.Context, txn interface{}, dto dtos.CreateUserDto) (dtos.CreatedUserDto, error) {
+	return dtos.CreatedUserDto{}, pst.useCaseError
 }
