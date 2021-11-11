6936f3fd913a8d286a70b94aa27eef8b0514e866
ralvescosta <rafael.rac.mg@gmail.com>
2021-11-11 16:49:47 -0300

    webapi:feat: telemetry

--- webapi/pkg/infra/telemetry/telemetry.go
+++ webapi/pkg/infra/telemetry/telemetry.go
@@ -1,17 +1,25 @@
 package telemetry
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/http"
+	"webapi/pkg/app/interfaces"
 
+	"github.com/gin-gonic/gin"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
 	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
 )
 
-func Config(service string) (opentracing.Tracer, io.Closer) {
+type telemetry struct {
+	tracer opentracing.Tracer
+	closer io.Closer
+}
+
+func NewTelemetry() interfaces.ITelemetry {
 	cfg, err := config.FromEnv()
 	if err != nil {
 		panic(fmt.Sprintf("ERROR: failed to read config from env vars: %v\n", err))
@@ -22,19 +30,57 @@ func Config(service string) (opentracing.Tracer, io.Closer) {
 	}
 	opentracing.SetGlobalTracer(tracer)
 
-	return tracer, closer
+	return &telemetry{tracer, closer}
+}
+
+func (pst *telemetry) Dispatch() {
+	pst.closer.Close()
+}
+
+func (telemetry) GinMiddle() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		span := opentracing.GlobalTracer().StartSpan(fmt.Sprintf("HTTP %s %s", ctx.Request.Method, ctx.Request.RequestURI))
+		defer span.Finish()
+
+		tracerCtx := opentracing.ContextWithSpan(ctx.Request.Context(), span)
+
+		ctx.Set("tracerCtx", tracerCtx)
+		ctx.Next()
+
+		responseStatusCode := ctx.Writer.Status()
+		span.SetTag("http.status_code", responseStatusCode)
+
+		if responseStatusCode >= http.StatusBadRequest {
+			span.SetTag("error", true)
+		}
+	}
+}
+
+const (
+	TAG_SQL_SELECT_USER = "SQL SELECT USER"
+	TAG_SQL_INSERT_USER = "SQL INSERT USER"
+)
+
+func (pst *telemetry) InstrumentQuery(ctx context.Context, sqlType string, sql string) opentracing.Span {
+	span := opentracing.SpanFromContext(ctx)
+	span = pst.tracer.StartSpan(sqlType, opentracing.ChildOf(span.Context()))
+	ext.SpanKindRPCClient.Set(span)
+	ext.PeerService.Set(span, "postgreSQL")
+	span.SetTag("sql.query", sql)
+
+	return span
 }
 
 // StartSpanFromRequest extracts the parent span context from the inbound HTTP request
 // and starts a new child span if there is a parent span.
-func StartSpanFromRequest(tracer opentracing.Tracer, header http.Header) opentracing.Span {
-	spanCtx, _ := Extract(tracer, header)
-	return tracer.StartSpan("ping-receive", ext.RPCServerOption(spanCtx))
+func (pst *telemetry) StartSpanFromRequest(header http.Header) opentracing.Span {
+	spanCtx, _ := pst.Extract(header)
+	return pst.tracer.StartSpan("ping-receive", ext.RPCServerOption(spanCtx))
 }
 
 // Inject injects the outbound HTTP request with the given span's context to ensure
 // correct propagation of span context throughout the trace.
-func Inject(span opentracing.Span, request *http.Request) error {
+func (telemetry) Inject(span opentracing.Span, request *http.Request) error {
 	return span.Tracer().Inject(
 		span.Context(),
 		opentracing.HTTPHeaders,
@@ -43,8 +89,8 @@ func Inject(span opentracing.Span, request *http.Request) error {
 
 // Extract extracts the inbound HTTP request to obtain the parent span's context to ensure
 // correct propagation of span context throughout the trace.
-func Extract(tracer opentracing.Tracer, header http.Header) (opentracing.SpanContext, error) {
-	return tracer.Extract(
+func (pst *telemetry) Extract(header http.Header) (opentracing.SpanContext, error) {
+	return pst.tracer.Extract(
 		opentracing.HTTPHeaders,
 		opentracing.HTTPHeadersCarrier(header))
 }
