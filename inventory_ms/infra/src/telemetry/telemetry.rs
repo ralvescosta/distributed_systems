5e8cb7d17e69f846036be12aac9fa1fffaf304a0
ralvescosta <rafael.rac.mg@gmail.com>
2021-11-14 14:46:09 -0300

    inventory:refactor: tracer

--- inventory_ms/infra/src/telemetry/telemetry.rs
+++ inventory_ms/infra/src/telemetry/telemetry.rs
@@ -1,8 +1,13 @@
 use std::{env, error::Error};
 
-use opentelemetry::{global, sdk::propagation::TraceContextPropagator};
+use opentelemetry::{
+    global,
+    propagation::{Extractor, Injector},
+    sdk::propagation::TraceContextPropagator,
+    Context,
+};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
+use tracing_opentelemetry::OpenTelemetrySpanExt;
 use tracing_subscriber::prelude::*;
 
 pub struct Telemetry {}
@@ -16,24 +21,64 @@ impl Telemetry {
             .with_service_name(app_name)
             .install_batch(opentelemetry::runtime::Tokio)?;
 
-        let formatting_layer = BunyanFormattingLayer::new(String::from("debug"), std::io::stdout);
+        let formatting_layer = BunyanFormattingLayer::new(String::from("info"), std::io::stdout);
         tracing_subscriber::registry()
-            .with(tracing_subscriber::EnvFilter::new("debug"))
+            .with(tracing_subscriber::EnvFilter::new("info"))
             .with(tracing_opentelemetry::layer().with_tracer(tracer))
             .with(JsonStorageLayer)
             .with(formatting_layer)
             .try_init()?;
 
-        // match LogTracer::init() {
-        //     Err(err) => {
-        //         println!("ERROR AQUI");
-        //         println!("{}", err.to_string())
-        //     }
-        //     _ => (),
-        // }
-
         Ok(())
     }
 
+    pub fn get_grpc_ctx<T>(request: &tonic::Request<T>) -> Context {
+        let ctx = global::get_text_map_propagator(|prop| {
+            prop.extract(&GrpcExtractor(request.metadata()))
+        });
+        tracing::Span::current().set_parent(ctx.clone());
+        ctx
+    }
+
+    pub fn inject_grpc_ctx<T>(mut request: tonic::Request<T>) {
+        global::get_text_map_propagator(|propagator| {
+            propagator.inject_context(
+                &tracing::Span::current().context(),
+                &mut GrpcInjector(request.metadata_mut()),
+            )
+        });
+    }
+
     pub fn span(&self) {}
+}
+
+struct GrpcExtractor<'a>(&'a tonic::metadata::MetadataMap);
+impl<'a> Extractor for GrpcExtractor<'a> {
+    /// Get a value for a key from the MetadataMap.  If the value can't be converted to &str, returns None
+    fn get(&self, key: &str) -> Option<&str> {
+        self.0.get(key).and_then(|metadata| metadata.to_str().ok())
+    }
+
+    /// Collect all the keys from the MetadataMap.
+    fn keys(&self) -> Vec<&str> {
+        self.0
+            .keys()
+            .map(|key| match key {
+                tonic::metadata::KeyRef::Ascii(v) => v.as_str(),
+                tonic::metadata::KeyRef::Binary(v) => v.as_str(),
+            })
+            .collect::<Vec<_>>()
+    }
+}
+
+struct GrpcInjector<'a>(&'a mut tonic::metadata::MetadataMap);
+impl<'a> Injector for GrpcInjector<'a> {
+    /// Set a key and value in the MetadataMap.  Does nothing if the key or value are not valid inputs
+    fn set(&mut self, key: &str, value: String) {
+        if let Ok(key) = tonic::metadata::MetadataKey::from_bytes(key.as_bytes()) {
+            if let Ok(val) = tonic::metadata::MetadataValue::from_str(&value) {
+                self.0.insert(key, val);
+            }
+        }
+    }
 }
