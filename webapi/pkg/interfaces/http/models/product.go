a9600ebc676c57e2cf85447f91f10b7a087d35ea
ralvescosta <rafael.rac.mg@gmail.com>
2021-11-20 07:53:03 -0300

    webapi:refactor: update product model

--- webapi/pkg/interfaces/http/models/product.go
+++ webapi/pkg/interfaces/http/models/product.go
@@ -16,6 +16,42 @@ type ProductModel struct {
 	UpdatedAt       string   `json:"updated_at"`
 }
 
-func ToGetByIdResponse(dto dtos.ProductDto) ProductModel {
-	return ProductModel{}
+type CreateProductModel struct {
+	ProductCategory string   `json:"product_category" validate:"required"`
+	Tag             string   `json:"tag" validate:"required"`
+	Title           string   `json:"title" validate:"required"`
+	Subtitle        string   `json:"subtitle" validate:"required"`
+	Authors         []string `json:"authors" validate:"required"`
+	AmountInStock   int      `json:"amount_in_stock" validate:"required"`
+	NumPages        int      `json:"num_pages" validate:"required"`
+	Tags            []string `json:"tags" validate:"required"`
+}
+
+func (pst CreateProductModel) ToProductDto() dtos.ProductDto {
+	return dtos.ProductDto{
+		ProductCategory: pst.ProductCategory,
+		Tag:             pst.Tag,
+		Title:           pst.Title,
+		Subtitle:        pst.Subtitle,
+		Authors:         pst.Authors,
+		AmountInStock:   pst.AmountInStock,
+		NumPages:        pst.NumPages,
+		Tags:            pst.Tags,
+	}
+}
+
+func ToProductResponse(dto dtos.ProductDto) ProductModel {
+	return ProductModel{
+		Id:              dto.Id,
+		ProductCategory: dto.ProductCategory,
+		Tag:             dto.Tag,
+		Title:           dto.Title,
+		Subtitle:        dto.Subtitle,
+		Authors:         dto.Authors,
+		AmountInStock:   dto.AmountInStock,
+		NumPages:        dto.NumPages,
+		Tags:            dto.Tags,
+		CreatedAt:       dto.CreatedAt,
+		UpdatedAt:       dto.UpdatedAt,
+	}
 }
