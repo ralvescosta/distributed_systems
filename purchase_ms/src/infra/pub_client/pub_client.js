672248d43bc08040248aa6dd87c9dc8402acd5db
ralvescosta <rafael.rac.mg@gmail.com>
2021-11-26 06:22:10 -0300

    purchase:refactor: pub client

--- purchase_ms/src/infra/pub_client/pub_client.js
+++ purchase_ms/src/infra/pub_client/pub_client.js
@@ -1,3 +1,11 @@
-class PubClient {}
+class PubClient {
+  constructor(logger) {
+    this.logger = logger;
+  }
+
+  updateInventory() {}
+  
+  purchaseEmail() {}
+}
 
 module.exports = { PubClient }
