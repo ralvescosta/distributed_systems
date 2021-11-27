ceb5afb6c7e2109e1f4af46aa26c862bd404ba75
ralvescosta <rafael.rac.mg@gmail.com>
2021-11-27 10:58:54 -0300

    purchase:refactor: grpc client

--- purchase_ms/src/infra/grpc_client/inventory_client.js
+++ purchase_ms/src/infra/grpc_client/inventory_client.js
@@ -1,12 +1,22 @@
+const { promisify } = require('util');
 const { right } = require('../../domain/entities/either')
+const { GrpcClient } = require('./grpc_client')
 
-class InventoryClient {
+class InventoryClient extends GrpcClient {
   constructor(logger) {
+    super();
     this.logger = logger;
+    this.loadProto('inventory.proto')
   }
 
-  verifyAvailability() {
-    this.logger.info("[InventoryClient::verifyAvailability]")
+  async verifyAvailability(id) {
+    const client = this.getClientInstance('inventory', 'Inventory', process.env.INVENTORY_MS_URL)
+    
+    if(client.isLeft())
+      return client
+
+    const result = await promisify(client.value.getProductById)({ id })
+    this.logger.info("[InventoryClient::verifyAvailability]", result)
     return right(true)
   }
 }
