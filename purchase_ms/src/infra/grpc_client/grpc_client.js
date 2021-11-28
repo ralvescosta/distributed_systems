a17a6cd2aa40e34f71bc674dedace34ad9ed2459
ralvescosta <rafael.rac.mg@gmail.com>
2021-11-28 15:34:36 -0300

    purchase:refactor: grpc client

--- purchase_ms/src/infra/grpc_client/grpc_client.js
+++ purchase_ms/src/infra/grpc_client/grpc_client.js
@@ -1,9 +1,12 @@
-const grpc = require('@grpc/grpc-js');
+const { loadPackageDefinition, credentials, status } = require('@grpc/grpc-js');
+
 const protoLoader = require('@grpc/proto-loader');
 const { resolve } = require('path')
 
 const { left, right } = require('../../domain/entities/either')
 const { UnavailableServiceError } = require('../../application/errors/unavailable_service_error')
+const { InternalError } = require('../../application/errors/internal_error')
+const { NotFoundError } = require('../../application/errors/not_found_error')
 
 class GrpcClient {
   constructor() {
@@ -12,7 +15,7 @@ class GrpcClient {
 
   loadProto(protoFile) {
     this.packageDefinition = protoLoader.loadSync(
-      resolve(__dirname, '..', 'proto', protoFile),
+      resolve(__dirname, 'proto', protoFile),
       {
         keepCase: true,
         longs: String,
@@ -24,22 +27,30 @@ class GrpcClient {
   }
 
   getClientInstance(pkg, client, host) {
-    const definition = grpc.loadPackageDefinition(this.packageDefinition)
+    const definition = loadPackageDefinition(this.packageDefinition)
     
     const grpcPackage = definition[pkg]
     if (!grpcPackage)
       return left(new UnavailableServiceError("Inventory Client - Unavailable service"))
 
-    const ClientClass = grpcPackage[client]
+    const GrpcClient = grpcPackage[client]
     if (!client) 
       return left(new UnavailableServiceError("Inventory Client - Unavailable service"))
 
     try {
-      right(new ClientClass(host, grpc.credentials.createInsecure()))
+      return right(new GrpcClient(host, credentials.createInsecure()))
     } catch (err) {
       return left(new UnavailableServiceError("Inventory Client - Unavailable service"))
     }
-    
+  }
+
+  errorMapper(err) {
+    switch (err.code) {
+      case status.NOT_FOUND:
+        return left(new NotFoundError("Register not found", err))
+      default:
+        return left(new InternalError("Some error occur", err))
+    }
   }
 }
 
