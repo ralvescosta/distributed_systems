10876fe728188d3f453fbc3ed50a24c78af2f408
ralvescosta <rafael.rac.mg@gmail.com>
2021-11-29 05:14:04 -0300

    purchase:refactor: telemetry

--- purchase_ms/src/infra/telemetry/telemetry.js
+++ purchase_ms/src/infra/telemetry/telemetry.js
@@ -23,14 +23,21 @@ class Telemetry {
     trace.setGlobalTracerProvider(tracer)
   }
 
-  instrumentAmqp(queue, exchange, routingKey) {
-    const cTracer = trace.getTracer(process.env.APP_NAME, '0.1.0');
+  instrumentAmqp({ queue, exchange, routingKey}) {
+    const cTracer = trace.getTracer(process.env.APP_NAME, process.env.APP_VERSION);
     const span = cTracer.startSpan(`Queue: ${queue}`);
 
     span.setAttribute("amqp.exchange", exchange)
     span.setAttribute("amqp.routingKey", routingKey)
 
     return span
+  }
+
+  createChildrenSpan({ context, query }) {
+    const cTracer = trace.getTracer(process.env.APP_NAME, process.env.APP_VERSION);
+    const span = trace.wrapSpanContext(context)
+
+    return cTracer.startActiveSpan(query, span) 
   }
 
   grpcInjector(context) {
@@ -40,8 +47,23 @@ class Telemetry {
     return metadata
   }
 
-  amqpExtractor(headers) {
-    return headers
+  amqpExtractor({ headers = {}, queue, exchange, routingKey }) {
+    const { traceparent } = headers
+    if(!traceparent)
+      return this.instrumentAmqp({ queue, exchange, routingKey })
+
+    const [, traceId, spanId] = traceparent.split('-')
+
+    const span = trace.wrapSpanContext({
+      spanId,
+      traceId,
+    })
+
+    span.updateName(queue)
+    span.setAttribute("amqp.exchange", exchange)
+    span.setAttribute("amqp.routingKey", routingKey)
+
+    return span
   }
 
   getTracer() {
