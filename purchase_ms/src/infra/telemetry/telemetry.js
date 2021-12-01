5b9202437a647b4ad045de9a1ac1a986f59e40b3
ralvescosta <rafael.rac.mg@gmail.com>
2021-12-01 06:04:10 -0300

    purchase:refactor: telemetry

--- purchase_ms/src/infra/telemetry/telemetry.js
+++ purchase_ms/src/infra/telemetry/telemetry.js
@@ -1,6 +1,6 @@
 const { Resource } = require('@opentelemetry/resources')
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions')
-const { trace, context: apiContext } = require('@opentelemetry/api')
+const { trace, context: apiContext, propagation } = require('@opentelemetry/api')
 const { BasicTracerProvider, BatchSpanProcessor } = require('@opentelemetry/tracing')
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger')
 const { Metadata } = require('@grpc/grpc-js');
@@ -8,12 +8,14 @@ const { Metadata } = require('@grpc/grpc-js');
 class Telemetry {
   constructor(logger) {
     this.logger = logger;
+    this.appName = process.env.APP_NAME || 'purchase-ms';
+    this.appVersion = process.env.APP_VERSION || '0.0.1'
   }
 
   start() {
     const tracer = new BasicTracerProvider({
       resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: process.env.APP_NAME
+        [SemanticResourceAttributes.SERVICE_NAME]: this.appName
       })
     })
     const exporter = new JaegerExporter({
@@ -24,7 +26,7 @@ class Telemetry {
   }
 
   instrumentAmqp({ queue, exchange, routingKey}) {
-    const cTracer = trace.getTracer(process.env.APP_NAME, process.env.APP_VERSION);
+    const cTracer = trace.getTracer(this.appName, this.appVersion);
     const span = cTracer.startSpan(`Queue: ${queue}`);
 
     span.setAttribute("amqp.exchange", exchange)
@@ -35,13 +37,13 @@ class Telemetry {
   }
 
   createChildrenSpan({ name, context }) {
-    const cTracer = trace.getTracer(process.env.APP_NAME, process.env.APP_VERSION);
+    const cTracer = trace.getTracer(this.appName, this.appVersion);
     
     return cTracer.startSpan(name, undefined, context)
   }
 
-  grpcInjector({ grpcCall, context }) {
-    const cTracer = trace.getTracer(process.env.APP_NAME, process.env.APP_VERSION);
+  grpcInjector({ grpcCall = '', context }) {
+    const cTracer = trace.getTracer(this.appName, this.appVersion);
 
     const span = cTracer.startSpan(grpcCall, undefined, context)
     const spanContext = span.spanContext()
@@ -52,25 +54,48 @@ class Telemetry {
     return { span, metadata }
   }
 
+  basicExtractor() {
+    return {
+      keys: (carrier) =>  {
+        return Object.keys(carrier).map((key) => [key, carrier[key]])
+      },
+      get: (carrier, key) => {
+        return carrier[key]
+      }
+    }
+  }
+
   amqpExtractor({ headers = {}, queue, exchange, routingKey }) {
     const { traceparent } = headers
     if(!traceparent)
       return this.instrumentAmqp({ queue, exchange, routingKey })
 
-    const [, traceId, spanId] = traceparent.split('-')
+    const cTracer = trace.getTracer(this.appName, this.appVersion);
 
-    const span = trace.wrapSpanContext({
-      spanId,
-      traceId,
-    })
-
-    span.updateName(queue)
+    const span = cTracer.startSpan(`amqpSubQueue: ${queue}`)
+    let context = trace.setSpan(apiContext.active(), span)
+    context = propagation.extract(
+      context,
+      headers,
+      this.basicExtractor
+    );
+    
     span.setAttribute("amqp.exchange", exchange)
     span.setAttribute("amqp.routingKey", routingKey)
 
-    const context = trace.setSpan(apiContext.active(), span)
-
     return { span, context }
+  }
+
+  amqpInjector({ queue, exchange, routingKey, context }) {
+    const cTracer = trace.getTracer(this.appName, this.appVersion);
+    const span = cTracer.startSpan(`amqpPubQueue: ${queue}`, undefined, context)
+
+    span.setAttribute("amqp.exchange", exchange)
+    span.setAttribute("amqp.routingKey", routingKey)
+
+    const spanContext = span.spanContext()
+
+    return { span,  headers: { traceparent: `00-${spanContext.traceId}-${spanContext.spanId}-01`} }
   }
 
   getTracer() {
