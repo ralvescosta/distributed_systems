309821d6a230e7124051e92cbda0502c421fae7a
ralvescosta <rafael.rac.mg@gmail.com>
2021-12-22 06:18:27 -0300

    webapi:refactor: broker

--- webapi/pkg/app/interfaces/i_message_broker.go
+++ webapi/pkg/app/interfaces/i_message_broker.go
@@ -5,7 +5,7 @@ import "context"
 type IMessageBroker interface {
 	Publisher(
 		ctx context.Context,
-		exchangeName, exchangeType, queueName, routingKey string,
+		exchangeName, exchangeType, queueName, routingKey, deadLetterExchange, deadLetterRoutingKey string,
 		body interface{},
 		header map[string]interface{},
 	) error
